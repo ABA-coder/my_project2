10ec522121a505cbce06b955fef4cb3255b7a39f
Абылай <erkenovabylaj@gmail.com>
2025-12-19 16:35:32 +0500

    my_first push

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
     public class Main{
         public static void main(String[]args){
 
-            Flight flight=new Flight("ABA101","London",1);
+            Flight flight=new Flight("ABA101",":London",1);
 
             Passenger p1=new Passenger("Dias","Serikov","AP101","ABA100");
             Passenger p2=new Passenger("Kamila","Amirova","AD109","ABA109");
