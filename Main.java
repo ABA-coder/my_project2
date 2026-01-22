fd12b890e39b9e26822905391f9601b0c52e2d3b
Абылай <erkenovabylaj@gmail.com>
2026-01-23 04:13:42 +0500

    assignment 3

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
+        DatabaseService db = new DatabaseService();
         Flight flight = new Flight("ABA101", "London", 2);
+
+        db.saveFlight(flight.getFlightNumber(), flight.getDestination(), flight.getCapacity());
 
         Passenger p1 = new Passenger("Dias", "Serikov", "AP101", "ABA101");
         Passenger p2 = new Passenger("Kamila", "Amirova", "AD109", "ABA101");
-
         CrewMember c1 = new CrewMember("Alex", "Brown", "Pilot");
 
         Booking b1 = new Booking(p1, flight);
         Booking b2 = new Booking(p2, flight);
+
+        db.updateCapacity(flight.getFlightNumber(), flight.getCapacity());
 
         b1.displayBooking();
         b2.displayBooking();
@@ -19,14 +22,9 @@ public class Main {
         service.addHuman(p2);
         service.addHuman(c1);
 
-        System.out.println("=== ALL PEOPLE ===");
-        service.showAll();
-
-        System.out.println("\n=== SORTED BY SURNAME ===");
         service.sortBySurname();
         service.showAll();
 
-        System.out.println("\n=== PASSENGERS ONLY ===");
-        service.showPassengersOnly();
+        db.readFlights();
     }
 }
